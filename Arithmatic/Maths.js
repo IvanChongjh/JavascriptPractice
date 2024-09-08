926629fa1d93ee127f8b92c70e309be5c4335d07
IvanChongjh <ivancjh.six@gmail.com>
2024-09-08 21:28:43 +0800

    Added maths

--- Arithmatic/Maths.js
+++ Arithmatic/Maths.js
@@ -1,5 +1,19 @@
+// declaring variables
 let i = 10;
 let x = 20;
 
+// performing sum
 let y = i + x;
 console.log(y);
+
+// performing subtraction
+let e = i - x;
+console.log(e);
+
+// performing multiplication
+let o = i * x;
+console.log(o);
+
+// performing division 
+let m = i/x;
+console.log(m);
