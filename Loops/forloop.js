211f410ae10747a589a28b3729e94e336a30cd60
IvanChongjh <ivancjh.six@gmail.com>
2024-09-12 02:29:14 +0800

    added forloop which populate contents using javascript

---
+++ Loops/forloop.js
@@ -0,0 +1,8 @@
+const cars = ["Cake", "Fries", "Burger", "Fish", "Beef", "Lamb"];
+
+let text = "";
+for (let i = 0; i < cars.length; i++) {
+  text += cars[i] + "<br>";
+}
+
+document.getElementById("demo").innerHTML = text;
