4d6516df72e93342ee4145573853067022dd76d6
IvanChongjh <ivancjh.six@gmail.com>
2024-09-12 02:41:37 +0800

    Added Events folder, with examples of event applied to button to show time onclick feature

--- Loops/forloop.js
+++ Loops/forloop.js
@@ -3,6 +3,6 @@ const cars = ["Cake", "Fries", "Burger", "Fish", "Beef", "Lamb"];
 let text = "";
 for (let i = 0; i < cars.length; i++) {
   text += cars[i] + "<br>";
+ 
 }
-
 document.getElementById("demo").innerHTML = text;
