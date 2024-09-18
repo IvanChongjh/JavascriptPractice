fa5101771ccce454f67b136126164bcf123d13da
IvanChongjh <ivancjh.six@gmail.com>
2024-09-18 23:07:59 +0800

    added comment on addtional features

--- Basics/Date.js
+++ Basics/Date.js
@@ -2,3 +2,13 @@
 // use node filename.js to run 
 const date = new Date();
 console.log(date);
+
+// other features 
+// new Date(date string)
+// new Date(year,month)
+// new Date(year,month,day)
+// new Date(year,month,day,hours)
+// new Date(year,month,day,hours,minutes)
+// new Date(year,month,day,hours,minutes,seconds)
+// new Date(year,month,day,hours,minutes,seconds,ms)
+// new Date(milliseconds)
